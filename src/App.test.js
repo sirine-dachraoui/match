5614481851239d6c2fae45152d0211ae3963db97
sirine-dachraoui <sirinedachraoui94@gmail.com>
2022-06-10 13:10:47 +0100

    readme --update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('shows adding a match form page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getBy("Add A Match");
   expect(linkElement).toBeInTheDocument();
 });
+
+// const server = setupServer(
+//   rest.get('/matches', (req, res, ctx) => {
+//     return res(ctx.json({greeting: 'hello there'}))
+//   }),
+// )
